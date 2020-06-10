e75f57a464c4606ada6c64cdd6eb98512424384e
chinguyen98 <dacchi6761@gmail.com>
2020-06-10 12:18:24 +0700

    update

--- resources/views/customers/intro.blade.php
+++ resources/views/customers/intro.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 
+<section class="home-slider owl-carousel">
 
+  <div class="slider-item" style="background-image: url(customers/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row slider-text justify-content-center align-items-center">
+
+        <div class="col-md-7 col-sm-12 text-center ftco-animate">
+          <h1 class="mb-3 mt-5 bread">{{$title}}</h1>
+          <p class="breadcrumbs"><span class="mr-2"><a href="/">Trang chủ</a></span> <span>{{$title}}</span></p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
 
 @endsection
