fc487beb0d6b9b2cf95da9ad8e35ca8d7159daa8
ChiNguyen <dacchi6761@gmail.com>
2020-06-29 20:54:50 +0700

    update reset password feature

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -26,5 +29,63 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/accounts';
+
+    public function reset(Request $request)
+    {
+        $request->validate($this->rules(), $this->validationErrorMessages(), $this->customAttributes());
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request),
+            function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+            ? $this->sendResetResponse($request, $response)
+            : $this->sendResetFailedResponse($request, $response);
+    }
+
+    protected function sendResetResponse(Request $request, $response)
+    {
+        if ($request->wantsJson()) {
+            return new JsonResponse(['message' => trans($response)], 200);
+        }
+
+        $request->session()->flash('success_message', 'Đặt lại mật khẩu thành công!');
+        return redirect($this->redirectPath())
+            ->with('status', trans($response));
+    }
+
+    private function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:8',
+        ];
+    }
+
+    private function validationErrorMessages()
+    {
+        return [
+            'confirmed' => 'Mật khẩu xác nhận không khớp!',
+            'min' => ':attribute phải trên 8 kí tự!',
+        ];
+    }
+
+    private function customAttributes()
+    {
+        return [
+            'email' => 'Địa chỉ email',
+            'password' => 'Mật khẩu',
+        ];
+    }
 }
