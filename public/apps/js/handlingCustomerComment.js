1b88ee11182fba64d087af658ac669e473797556
ChiNguyen <dacchi6761@gmail.com>
2020-07-13 20:26:22 +0700

    scale image before upload

--- public/apps/js/handlingCustomerComment.js
+++ public/apps/js/handlingCustomerComment.js
@@ -9,6 +9,7 @@ const ratingErrArea = document.querySelector('.rating-err');
 const contentErrArea = document.querySelector('.commentContent-err');
 const imagesErrArea = document.querySelector('.commentImage-err');
 const previewImageArea = document.querySelector('.previewImageArea');
+let scaleImage = [];
 
 function validateForm(rating, content) {
     let flag = true;
@@ -29,7 +30,6 @@ async function handlingAddNewComment() {
     const rating = document.querySelector('#rating input[type="radio"]:checked')?.value;
     const title = commentTitleInput.value;
     const content = commentContentInput.value;
-    const images = Object.values(commentImageInput.files);
     if (!validateForm(rating, content)) {
         return;
     }
@@ -38,7 +38,7 @@ async function handlingAddNewComment() {
     formData.append('rating', rating);
     formData.append('title', title);
     formData.append('content', content);
-    images.forEach(image => {
+    scaleImage.forEach(image => {
         formData.append('image[]', image);
     });
 
@@ -50,7 +50,7 @@ async function handlingAddNewComment() {
         method: 'POST',
         credentials: 'same-origin',
         body: formData,
-        headers:{
+        headers: {
             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
         }
     }).then(res => res.json()).then(dataJson => dataJson);
@@ -61,6 +61,7 @@ async function handlingAddNewComment() {
 function handlingPreviewImage() {
     imagesErrArea.innerHTML = '';
     previewImageArea.innerHTML = '';
+    scaleImage.length = 0;
 
     if (this.files) {
         if (this.files.length > 5) {
@@ -69,7 +70,7 @@ function handlingPreviewImage() {
             return;
         }
 
-        [...this.files].forEach(file => {
+        [...this.files].forEach((file, index) => {
             if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
                 imagesErrArea.innerHTML = 'Vui lòng upload đúng định dạng hình ảnh!';
                 return;
@@ -85,6 +86,21 @@ function handlingPreviewImage() {
                 image.style.borderRadius = '20%';
                 image.style.margin = '0 0.2rem';
                 previewImageArea.appendChild(image);
+
+                image.addEventListener('load', (e) => {
+                    const canvas = document.createElement('canvas');
+                    const MAX_WIDTH = 175;
+
+                    const scaleSize = MAX_WIDTH / e.target.width;
+                    canvas.width = MAX_WIDTH;
+                    canvas.height = e.target.height * scaleSize;
+
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height);
+
+                    const srcEncoded = ctx.canvas.toDataURL(e.target, 'image/jpeg');
+                    scaleImage.push(srcEncoded);
+                })
             })
 
             fileReader.readAsDataURL(file);
