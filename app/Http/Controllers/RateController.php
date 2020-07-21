098353fd31ffee6e9978a362ac64b2fb4fb7dd68
ChiNguyen <dacchi6761@gmail.com>
2020-07-21 12:10:37 +0700

    recently buy rating

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RateController extends Controller
 {
     public function index()
     {
+        $coffees = DB::table('orders')
+            ->join('order_details', 'orders.id', '=', 'order_details.id_order')
+            ->join('coffees', 'coffees.id', '=', 'order_details.id_coffee')
+            ->where('orders.id_customer', Auth::user()->id)
+            ->distinct('coffees.id')
+            ->get([
+                'coffees.slug',
+                'coffees.image',
+                'coffees.name',
+            ]);
+
         return view('customers.rates.index')->with([
             'title' => 'Nhận xét sản phẩm đã mua',
+            'coffees' => $coffees,
         ]);
     }
 }
--- resources/views/customers/rates/index.blade.php
+++ resources/views/customers/rates/index.blade.php
@@ -7,17 +7,39 @@
         <div class="overlay"></div>
         <div class="container">
             <div class="row slider-text justify-content-center align-items-center">
-
                 <div class="col-md-7 col-sm-12 text-center ftco-animate">
                     <h1 class="mb-3 mt-5 bread">Nhận xét sản phẩm đã mua</h1>
                     <p class="breadcrumbs"><span class="mr-2"><a href="/">Trang chủ</a></span> <span>Nhận xét sản phẩm đã mua</span></p>
-
                 </div>
-
             </div>
         </div>
     </div>
+</section>
 
+<section class="ftco-menu">
+    <div class="container">
+        <h3>Nhận xét sản phẩm đã mua</h3>
+        <div class="dmsp-main-container">
+            <div class="pt-3 row dmsp-main-container__list d-lg-flex flex-wrap">
+                @foreach($coffees as $coffee)
+
+                <div class="dmsp-main-container__item col-sm-12 col col-md-3 pt-3 text-center  d-sm-flex d-lg-flex flex-column justify-content-center align-items-center">
+                    <a href="{{route('customer.coffees.show', ['slug'=>$coffee->slug]) . '#flag'}}"><img src="apps/images/coffees/{{$coffee->image}}" alt=""></a>
+                    <a href="{{route('customer.coffees.show', ['slug'=>$coffee->slug]) . '#flag'}}">
+                        <div class="row">
+                            <div title="{{$coffee->name}}" class="coffeeName mt-3 col-md-12 text-center text-truncate">
+                                {{$coffee->name}}
+                            </div>
+                        </div>
+                    </a>
+
+                    <p><a href="{{route('customer.coffees.show', ['slug'=>$coffee->slug]) . '#flag'}}" class="btn btn-primary btn-outline-primary">XEM SẢN PHẨM</a></p>
+                </div>
+
+                @endforeach
+            </div>
+        </div>
+    </div>
 </section>
 
 @endsection
