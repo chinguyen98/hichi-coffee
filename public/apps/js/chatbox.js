48bef6152a92967602cc576f6fba4d224b1d6448
ChiNguyen <dacchi6761@gmail.com>
2020-08-12 19:30:14 +0700

    update GUI chatbox

--- public/apps/js/chatbox.js
+++ public/apps/js/chatbox.js
@@ -8,4 +8,10 @@ function openChatboxDialog() {
     chatboxWave.classList.add('d-none');
 }
 
+function closeChatboxDialogConversation(){
+    chatboxDialog.classList.add('d-none')
+    chatboxIcon.classList.remove('d-none');
+    chatboxWave.classList.remove('d-none');
+}
+
 chatboxIcon.addEventListener('click', openChatboxDialog);
--- resources/views/inc/customers/chatbox.blade.php
+++ resources/views/inc/customers/chatbox.blade.php
@@ -1,9 +1,13 @@
 <div class="chatbox-dialog d-none">
+    <div class="d-flex flex-row justify-content-between align-items-center">
+        <span>Hichi-Coffee</span>
+        <button onclick="closeChatboxDialogConversation()" class="btn btn-danger">X</button>
+    </div>
     <div class="chatbox-diaglog__conversation">
 
     </div>
-    <div class="d-flex justify-content-center align-items-center">
-        <input type="text">
+    <div class="d-flex justify-content-center align-items-center mb-2">
+        <input type="text" placeholder="Nhập gì đó đi">
         <button class="btn btn-primary ml-2">Gửi</button>
     </div>
 </div>
