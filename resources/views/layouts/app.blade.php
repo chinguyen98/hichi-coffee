935d890745256aac1a6abf3b7b6dfbecf599cebe
ChiNguyen <dacchi6761@gmail.com>
2020-08-30 10:10:40 +0700

    close chatbox

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,8 +58,6 @@
 
     @include('inc.customers.footer')
 
-    @include('inc.customers.chatbox')
-
     <!-- loader -->
     <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
             <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
