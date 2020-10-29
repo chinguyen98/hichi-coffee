43d0972a55c17ab3a87077f69383597976623ad7
chinguyen98 <dacchi6761@gmail.com>
2020-10-29 08:37:14 +0700

    command laravel test

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:test1 {var=1}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test command in laravel!';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        echo $this->argument('var');
+        return 'test';
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Coffee;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,54 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('test:test2 {var=123} {var2=456} {--opt=123}', function ($var2, $var, $opt) {
+    echo "{$var} - {$var2} {$opt}\n";
+})->describe('test2 ne');
+
+Artisan::command('test:test3 {arr*}', function ($arr) {
+    foreach ($arr as $item) {
+        echo "{$item} - ";
+    }
+})->describe('test3 ne');
+
+Artisan::command('test:test4 {--id=*}', function ($id) {
+    foreach ($id as $item) {
+        echo "{$item} - ";
+    }
+})->describe('test3 ne');
+
+Artisan::command('test:test5', function () {
+    $name = $this->ask('what your name!');
+    $this->line("Your name is {$name}!");
+    $age = $this->anticipate('what age?', ['qweqweqwe', 'asdas', 'xcz']);
+    $this->error($age);
+    $choice = $this->choice(
+        'Test choice',
+        ['choice1', 'choice2'],
+        1,
+    );
+    $this->info($choice);
+});
+Artisan::command('test:table', function () {
+    $coffees = Coffee::limit(10)->get(['name','price'])->toArray();
+    //$coffees = DB::table('coffees')->limit(10)->get(['name', 'price'])->toArray();
+    $headers = ['Name', 'Price'];
+    $this->table($headers, $coffees);
+});
+
+Artisan::command('test:progress', function () {
+    $users = Coffee::all();
+
+    $bar = $this->output->createProgressBar(count($users));
+
+    $bar->start();
+
+    foreach ($users as $key => $user) {
+        //$this->performTask($user);
+
+        $bar->advance($key);
+    }
+
+    $bar->finish();
+});
